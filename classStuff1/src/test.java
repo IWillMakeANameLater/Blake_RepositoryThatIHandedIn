1f71aff3ee3a0421413316136721b8e7934210a6
I will make a name later <AnOnlineMailAddress@gmail.com>
2021-11-10 14:02:32 -0800

    added assignment because i needed to

---
+++ classStuff1/src/test.java
@@ -0,0 +1,34 @@
+public class test {
+    public static void main (String args[]){
+        System.out.println("No");
+
+        // Operands
+        // These are primitive data
+        // Integers, Floats or Doubles, Booleans, and Characters
+
+        // Operators -------------
+        // Addition ( + )
+        // Subtraction ( - )
+        // Multiplication ( * )
+        // Division ( / )
+        // Modulus ( % )
+        // Increment ( += )
+        // Decrement ( -= )
+
+        // Order of Operations still apply
+        System.out.println(4 + 6 * 3);
+        // Integer Math
+        System.out.println(10/3);
+        // Operations with only integers will result in integers
+        // Built in Methods
+
+        System.out.println(5.0);
+        System.out.println(Math.sqrt(25) + 10 * (4 - 6));
+        // sqrt() is a built in method for finding square roots
+        // It doesnt matter how it is written, if two values evaluate to the same thing they will be the same to the computer.
+        System.out.println(Math.sqrt(Math.pow(2,10)) + 1);
+        System.out.println(10 + 1);
+        // pow() is a built in method for finding squares
+        // These above two lines evaluate to the samething even though they are written differently
+    }
+}
